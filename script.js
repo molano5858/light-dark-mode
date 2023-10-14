f79b436204feec8ed7b26339432901173889a1a1
molano5858 <molano5858@gmail.com>
2023-10-14 18:17:46 -0500

    creating structure html and styles , adding functionalities to switch light to dark mode

---
+++ script.js
@@ -0,0 +1,56 @@
+// selecting checkbox
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+// selecting all needed elements
+const nav = document.getElementById("nav");
+const toggleIcon = document.getElementById("toggle-icon");
+const image1 = document.getElementById("image1");
+const image2 = document.getElementById("image2");
+const image3 = document.getElementById("image3");
+const textBox = document.getElementById("text-box");
+
+// Dark mode styles
+
+function darkMode() {
+  nav.style.backgroundColor = "rgb(0 0 0/50%)";
+  textBox.style.backgroundColor = "rgb(255 255 255 /10%)";
+  textBox.style.color = "white";
+  // changing nav elements
+  toggleIcon.children[0].textContent = "Dark mode";
+  toggleIcon.children[1].classList.remove("fa-sun");
+  toggleIcon.children[1].classList.add("fa-moon");
+  // changing illustrations
+  image1.src = "./img/undraw_proud_coder_dark.svg";
+  image2.src = "./img/undraw_feeling_proud_dark.svg";
+  image3.src = "./img/undraw_conceptual_idea_dark.svg";
+}
+
+// Light Mode Styles
+function lightMode() {
+  nav.style.backgroundColor = "rgb(255 255 255 /10%)";
+
+  textBox.style.backgroundColor = "rgb(0 0 0/50%)";
+  textBox.style.color = " var(--on-primary)";
+  // changing nav elements
+  toggleIcon.children[0].textContent = "Light mode";
+  toggleIcon.children[1].classList.remove("fa-moon");
+  toggleIcon.children[1].classList.add("fa-sun");
+  // changing illustrations
+  image1.src = "./img/undraw_proud_coder_light.svg";
+  image2.src = "./img/undraw_feeling_proud_light.svg";
+  image3.src = "./img/undraw_conceptual_idea_light.svg";
+}
+
+// Change theme
+function switchTheme(event) {
+  console.log("se ejecuto");
+  if (event.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+    darkMode();
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+    lightMode();
+  }
+}
+
+// Switch´s listener
+toggleSwitch.addEventListener("change", switchTheme);
